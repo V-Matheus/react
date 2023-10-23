2a639656fff8487b5af9cdf6d221497a4f5c93ab
V-Matheus <victormatheus507@gmail.com>
2023-10-23 17:10:48 -0300

    componentes radio

--- formularios/componentesForm/src/App.jsx
+++ formularios/componentesForm/src/App.jsx
@@ -1,15 +1,28 @@
 import React from 'react';
 import Input from './Form/input';
+import Radio from './Form/Radio';
 import Select from './Form/Select';
 
 const App = () => {
   const [nome, setNome] = React.useState('');
   const [email, setEmail] = React.useState('');
-  const [produto, setProduto] = React.useState('')
+  const [produto, setProduto] = React.useState('');
+  const [cor, setCor] = React.useState('');
+  const [frutas, setFrutas] = React.useState('');
 
   return (
     <form>
-      <Select options={['smartphone', 'tablet']} value={produto} setValue={setProduto}/>
+      <h2>Cores</h2>
+      <Radio options={['Azul', 'Vermelho']} value={cor} setValue={setCor}/>
+
+      <h2>Frutas</h2>
+      <Radio options={['limão', 'Laranja', 'uva']} value={frutas} setValue={setFrutas}/>
+
+      <Select
+        options={['smartphone', 'tablet']}
+        value={produto}
+        setValue={setProduto}
+      />
       <Input id="nome" label="Nome" value={nome} setValue={setNome} />
       <Input
         id="email"
