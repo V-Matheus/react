27f9f677d6affd44f55607ce2ee7e7019c686af4
V-Matheus <victormatheus507@gmail.com>
2023-10-24 17:12:10 -0300

    css import

---
+++ css/importCss/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Tittle from './Tittle'
+
+const App = () => {
+  return <div className='container'>
+
+  </div>;
+};
+
+export default App;
---
+++ css/importCss/src/Tittle.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Tittle.css'
+
+const Tittle = () => {
+  return (
+    <h1>Tittle</h1>
+  )
+}
+
+export default Tittle
