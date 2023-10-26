2605d4d57b8e0cc035341c27c18e3fc2c7d64085
V-Matheus <victormatheus507@gmail.com>
2023-10-26 16:27:02 -0300

    img

---
+++ css/imagem/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import foto from './img/foto.jpg';
+import DogSVG from './DogSVG';
+
+const App = () => {
+  const [olho, setOlho] = React.useState(0);
+
+  function handleClick() {
+    for (let i = 0; i < 6; i++) {
+      setTimeout(() => {
+        setOlho(i);
+      }, 30 * i);
+    }
+  }
+
+  return (
+    <div>
+      <p className="fundo" onClick={handleClick}></p>
+      <DogSVG color="#84e" olho={olho} />
+      <img src={foto} alt="Cachorro" />
+    </div>
+  );
+};
+
+export default App;
