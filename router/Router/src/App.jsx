bb705602f8341aa617d95f1d86006ef37f95c126
V-Matheus <victormatheus507@gmail.com>
2023-10-27 16:05:15 -0300

    useParams

--- router/Router/src/App.jsx
+++ router/Router/src/App.jsx
@@ -6,6 +6,7 @@ import Header from './Header';
 import Footer from './Footer';
 import NaoEncontrado from './NaoEncontrado';
 import Login from './Login'
+import Produto from './Produto';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="sobre" element={<Sobre />} />
         <Route path="login" element={<Login />} />
+        <Route path="produto/:id" element={<Produto />} />
         <Route path="*" element={<NaoEncontrado />} />
       </Routes>
       <Footer />
--- router/Router/src/Header.jsx
+++ router/Router/src/Header.jsx
@@ -1,21 +1,18 @@
 import React from 'react';
 // import './Header.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
 const Header = () => {
+  const location = useLocation();
+  React.useEffect(() => {
+    console.log('Mudou a rota');
+  }, [location]);
   return (
     <nav>
       <NavLink to="/" end>
         Home
       </NavLink>{' '}
-      |{' '}
-      <NavLink to="sobre">
-        Sobre
-      </NavLink>
-      |{' '}
-      <NavLink to="login">
-        Login
-      </NavLink>
+      | <NavLink to="sobre">Sobre</NavLink>| <NavLink to="login">Login</NavLink>
     </nav>
   );
 };
