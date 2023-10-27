355b652ab7b9dd138883ed4d3b0a06b3405b2b7a
V-Matheus <victormatheus507@gmail.com>
2023-10-27 16:41:48 -0300

    head tirando erro do link do bootstrap

--- router/Router/src/Home.jsx
+++ router/Router/src/Home.jsx
@@ -10,12 +10,12 @@ const Home = () => {
       <Helmet>
         <title>Dogs | Home</title>
         <meta name="description" content="Essa é a descrição da Home" />
-        <link
+        {/* <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
           integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
           crossorigin="anonymous"
-        ></link>
+        ></link> */}
       </Helmet>
       <h1>Home</h1>
       <p>Essa é a home</p>
