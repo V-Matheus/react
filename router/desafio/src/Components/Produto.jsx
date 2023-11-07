669abb65af58dc3449bcbe70d81c23d3e804ed3f
V-Matheus <victormatheus507@gmail.com>
2023-11-07 15:53:09 -0300

    desafio router - produto

--- router/desafio/src/Components/Produto.jsx
+++ router/desafio/src/Components/Produto.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import styles from './Produto.module.css'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import styles from './Produto.module.css';
+import Head from './Head'
 
 const Produto = () => {
-  return (
-    <div className={styles.produto}>
-      <h1>Produto</h1>
-    </div>
-  )
-}
+  const [produto, setProduto] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+  const { id } = useParams();
 
-export default Produto
+  React.useEffect(() => {
+    async function fetchProduto(url) {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        const json = await response.json();
+        setProduto(json);
+      } catch (erro) {
+        setError('Um erro ocorreu');
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchProduto(`https://ranekapi.origamid.dev/json/api/produto/${id}`);
+  }, [id]);
+
+  if (loading) return <div>Carregando...</div>;
+  if (error) return <p>{error}</p>;
+  if (produto === null) return null;
+
+  return (
+    <section className={styles.produto + ' animeLeft'}>
+      <Head title={`Ranek | ${produto.nome}`} description={`Ranek | Esse é um produto: ${produto.nome}`}/>
+      {produto.fotos.map((foto) => (
+        <img key={foto.src} src={foto.src} alt={foto.titulo} />
+      ))}
+      <div>
+        <h1>{produto.nome}</h1>
+        <span className={styles.preco}>R$ {produto.preco}</span>
+        <p className={styles.descricao}>{produto.descricao}</p>
+      </div>
+    </section>
+  );
+};
+
+export default Produto;
