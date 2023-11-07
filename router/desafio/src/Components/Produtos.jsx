3cd4a87ed5219d96383fed959de2883b47259e80
V-Matheus <victormatheus507@gmail.com>
2023-11-06 21:18:07 -0300

    desafio route - dando fetch da api e criando uma rota de cada produto

--- router/desafio/src/Components/Produtos.jsx
+++ router/desafio/src/Components/Produtos.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import styles from './Produtos.module.css'
+import React from 'react';
+import styles from './Produtos.module.css';
+import {Link} from 'react-router-dom'
 
 const Produtos = () => {
-  return (
-    <div className={styles.produtos}>
-      <h1>Produtos</h1>
-    </div>
-  )
-}
+  const [produtos, setProdutos] = React.useState(null);
 
-export default Produtos
+  React.useEffect(() => {
+    fetch('https://ranekapi.origamid.dev/json/api/produto')
+      .then((r) => r.json())
+      .then((json) => setProdutos(json));
+  }, []);
+
+  if (produtos === null) return null;
+  return (
+    <section className={styles.produtos + ' animeLeft'}>
+      {produtos.map((produto) => (
+        <Link to={`produto/${produto.id}`} key={produto.id}>
+          <img src={produto.fotos[0].src} alt={produto.fotos[0].titulo} />
+          <h1 className={styles.nome}>{produto.nome}</h1>
+        </Link>
+      ))}
+    </section>
+  );
+};
+
+export default Produtos;
